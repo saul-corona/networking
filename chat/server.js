5b973864f875f40a3e632c0368f5c6b015137eb8
Saul Corona <saulcoronascs@gmail.com>
2025-03-12 18:20:38 -0600

    update server and client for chat app

--- chat/server.js
+++ chat/server.js
@@ -2,10 +2,21 @@ const net = require("net");
 
 const server = net.createServer();
 
-server.on('connection', (socket) => {
-  console.log("A new connection to the server")
+// an array of client sockets
+const clients = [];
+
+server.on("connection", socket => {
+  console.log("A new connection to the server!");
+
+  socket.on("data", data => {
+    clients.map(s => {
+      s.write(data);
+    });
+  });
+
+  clients.push(socket);
 });
 
 server.listen(3008, "127.0.0.1", () => {
-	console.log("opened server on", server.address());
+  console.log("opened server on", server.address());
 });
