64d9e17bd9c82894a21f30a73499a36ab06f835b
Saul Corona <saulcoronascs@gmail.com>
2025-03-15 11:59:50 -0600

    Add chat app

--- chat/client.js
+++ chat/client.js
@@ -6,20 +6,43 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    })
+  })
+}
+
+const ask = async () => {
+  const message = await rl.question("Enter a message > ");
+  await moveCursor(0, -1);
+  // clear the current line that the cursor is
+  await clearLine(0);
+  socket.write(message);
+};
+
 const socket = net.createConnection(
   { host: "127.0.0.1", port: 3008 },
   async () => {
     console.log("Connected to the server!");
-    const ask = async () => {
-      const message = await rl.question("Enter a message > ");
-      socket.write(message);
-    };
     ask();
   }
 );
 
-socket.on("data", data => {
+socket.on("data", async (data) => {
+  await moveCursor(0, -1);
+  await clearLine(0);
   console.log(data.toString("utf-8"));
+  ask();
 });
 
 socket.on("end", () => {
