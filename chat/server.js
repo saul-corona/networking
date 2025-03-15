936b24bdfc769e2f9c44403b54e58fdf6519aadb
Saul Corona <saulcoronascs@gmail.com>
2025-03-15 14:02:46 -0600

    Complete basic chat app

--- chat/server.js
+++ chat/server.js
@@ -5,16 +5,23 @@ const server = net.createServer();
 // an array of client sockets
 const clients = [];
 
-server.on("connection", socket => {
+server.on("connection", (socket) => {
   console.log("A new connection to the server!");
 
-  socket.on("data", data => {
-    clients.map(s => {
-      s.write(data);
+  const clientId = clients.length + 1;
+  socket.write(`id-${clientId}`);
+
+  socket.on("data", (data) => {
+    const dataString = data.toString("utf-8");
+    const id = dataString.substring(0, dataString.indexOf("-"));
+    const message = dataString.substring(dataString.indexOf("-message-") + 9);
+
+    clients.map((client) => {
+      client.socket.write(`> User ${id}: ${message}`);
     });
   });
 
-  clients.push(socket);
+  clients.push({ id: clientId.toString(), socket });
 });
 
 server.listen(3008, "127.0.0.1", () => {
